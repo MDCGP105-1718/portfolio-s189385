42968711421e2db7fec5b99bcb5dc73991be5434
s189385 <32356939+s189385@users.noreply.github.com>
2017-10-19 23:51:55 +0100

    these are some more python files i have created

---
+++ xp13.py
@@ -0,0 +1,40 @@
+"""nights = int(input("Please input the number of nights you wish to stay "))
+hotel_cost = nights * 70
+NewYork = 2000
+Auckland = 790
+Venice = 154
+Glasgow = 65
+
+destination = str(input("Please input your destination "))
+class_ = float(input("please input the class in which you wish to fly "))
+
+print(f"You are flying to {destination} in {class_} class")
+
+if class_ == 1:
+	class_ = class_ * 1.9
+elif class_ == 2:
+	class_ = class_ * 1.6
+elif class_ == 3:
+	class_ = class_ * 1.3
+else:
+	class_ = class_ 
+
+if destination = NewYork:
+	return NewYork """
+
+rental_days = float(input("Please enter the number of days you wish to rent the car for "))
+cost_of_car = rental_days * 30
+
+if rental_days > 7:
+	cost_of_car = cost_of_car - 50
+elif rental_days > 3 < 7:
+	cost_of_car = cost_of_car - 30 
+
+print(cost_of_car)
+
+
+
+
+
+
+
