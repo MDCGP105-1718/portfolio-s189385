cc366d85c08949dff9b513021a43658c37798577
s189385 <s189385@uos.ac.uk>
2017-11-09 23:15:43 +0000

    My second commit for my game

--- Game.py
+++ Game.py
@@ -1,6 +1,12 @@
-""" The Following are lists, strings and other data sets used within the game"""
-Room1 = "An empty room"
-Room2 = "A room that is not empty"
+import sys,time
+""" these are data types which store information such as the room desriptions, score and player inventory"""
+Room1 = "You are in a small room with no windows and a flickering light above you"
+Room2 = "room 2"
+Room3 = "room 3"
+Room4 = "room 4"
+Room5 = "room 5"
+
+""" These are a list of functions i have created which allow me to save time by not having to re-write code"""
 
 def DescribeRoom(CurrentRoom):
 	
@@ -8,3 +14,42 @@ def DescribeRoom(CurrentRoom):
 		print(Room1)
 	elif CurrentRoom == Room2:
 		print(Room2)
+	elif CurrentRoom == Room3:
+		print(Room3)
+	elif CurrentRoom == Room4:
+		print(Room3)
+	elif CurrentRoom == Room5:
+		print(Room3)
+
+
+def CurrentAction():
+
+	Action = input("What would you like to do? ")
+
+	if Action == "Describe":
+		DescribeRoom(CurrentRoom)
+
+
+def print_slow(str):
+    for letter in str:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(0.05)
+	
+
+""" This is where the game code begins"""
+
+#CurrentRoom must be defined in order for the Describe room function to work
+
+CurrentRoom = Room1
+
+print_slow("'Well hello there, whats your name?' ")
+
+Name = input("")
+
+print_slow(f"'well {Name} I am your keeper and you\'re stuck with me now'\n")
+
+print("The unknown man pulls the sack that has been covering your head and walks away laughing leaving you in an locked cell")
+
+CurrentAction()
+
