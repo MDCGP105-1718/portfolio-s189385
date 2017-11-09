0037613e9cb5701e1de2eb55dd3794ebee6485b3
s189385 <32356939+s189385@users.noreply.github.com>
2017-11-09 21:16:58 +0000

    nout

---
+++ Game.py
@@ -0,0 +1,10 @@
+""" The Following are lists, strings and other data sets used within the game"""
+Room1 = "An empty room"
+Room2 = "A room that is not empty"
+
+def DescribeRoom(CurrentRoom):
+	
+	if CurrentRoom == Room1:
+		print(Room1)
+	elif CurrentRoom == Room2:
+		print(Room2)
