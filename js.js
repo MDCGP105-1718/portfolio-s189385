8c842ec6f6e8b36b983b6a7c429d386924861512
s189385 <32356939+s189385@users.noreply.github.com>
2018-01-03 23:54:46 +0000

    added some layout changes

--- js.js
+++ js.js
@@ -1,5 +1,10 @@
-function Color() {
-	var y = document.getElementsByClassName("NavContainer");
-	
-	y[0].style.backgroundColor = "red";
+function DropDown() {
+	 var x = document.getElementsByClassName("NavContainer");
+    
+    if (x[0].style.display === "none") {
+    	 x[0].style.display = "flex";
+    	} else {
+    		 x[0].style.display = "none";
+    	}
+   
 }
