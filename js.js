8e76cfacd72fe728a42d651383a3bb456e776fc5
unknown <s189385@uos.ac.uk>
2018-01-12 18:56:17 +0000

    a

--- js.js
+++ js.js
@@ -1,5 +1,11 @@
-function Color() {
-	var y = document.getElementsByClassName("NavContainer");
-	
-	y[0].style.backgroundColor = "red";
+function DropDown() {
+	 var x = document.getElementsByClassName("NavContainer");
+    
+    if (x[0].style.display === "none") {
+    	 x[0].style.display = "flex";
+    	} else {
+    		 x[0].style.display = "none";
+    	}
+   
 }
+
